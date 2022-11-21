b16564ef84cfd6ae00b02d947ffdc4a4fdcdff1e
AleMoreiraCodes <104038381+AleMoreiraCodes@users.noreply.github.com>
2022-11-21 15:49:51 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdlib> ///rand()
+#include <ctime>   ///time()
+#include "ListaDupla.h"
+
+using namespace std;
+
+int numAleatorio(int a, int b)
+{
+    return a + rand()%(b - a + 1); /// retorna um numero inteiro aleat—rio entre a e b
+}
+
+int main()
+{
+    ListaDupla l; /// cria lista vazia
+    int NumNos = 10;
+
+    srand(time(NULL));
+
+    /// ATENCAO: os valores inseridos na lista sao gerados
+    /// aleatoriamente e mudam em cada execucao do programa!!!
+    cout << "Inserindo valores: ";
+    for(int i = 1; i <= NumNos; i++)
+    {
+        int val =  numAleatorio(0, 50); /// cria um valor aleat—rio entre 0 e 50
+        cout << val << ", ";
+        l.insereInicio(val);
+    }
+    cout << endl;
+
+    bool existe = l.busca(20);
+    cout << "O valor 20 esta na lista? ";
+    if(existe)
+        cout << "Sim" << endl;
+    else
+        cout << "Nao" << endl;
+    l.imprime();
+    l.insereFinal(10);
+    l.imprime();
+    l.insereFinal(67);
+    l.imprime();
+    l.insereFinal(43);
+    l.imprime();
+    l.removeFinal();
+    l.imprime();
+    l.imprimeReverso();
+    return 0;
+};
